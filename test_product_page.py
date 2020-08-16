3cf5c84891ad6fa9175b4bc48967f642b9af2ffb
mkshm97 <max.korchagin97@gmail.com>
2020-08-16 16:28:15 +0300

    final changes before the review

--- test_product_page.py
+++ test_product_page.py
@@ -7,6 +7,16 @@ import random
 
 
 @pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_add_to_basket_message()
+
+
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
